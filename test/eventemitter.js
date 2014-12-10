af3bb4102fd0a9f38b078522557aa2acffb0eec9
petea <smart__petea@mail.ru>
2014-12-10 07:45:22 +0200

    close #34

--- test/eventemitter.js
+++ test/eventemitter.js
@@ -2,46 +2,83 @@ var EE = require('../lib/eventemitter');
 var should = require('should');
 
 describe('EventEmitter', function() {
-	describe('events - newListener', function() {
-		it('check arguments', function() {
-			var ee = new EE;
-			var eventName = 'eventName';
-			var eventListener = function(){};
+	describe('events', function() {
+		describe('newListener', function() {
+			it('check arguments', function() {
+				var ee = new EE;
+				var eventName = 'eventName';
+				var eventListener = function(){};
 
-			ee.on('newListener', function(evName, evListener) {
-				evName.should.equal(eventName);
-				evListener.should.equal(eventListener);
+				ee.on('newListener', function(evName, evListener) {
+					evName.should.equal(eventName);
+					evListener.should.equal(eventListener);
+				});
+
+				ee.on(eventName, eventListener);
 			});
 
-			ee.on(eventName, eventListener);
+			it('same type of listeners - fired once', function() {
+				var ee = new EE;
+				var eventName = 'eventName';
+				var evL1 = function(){};
+				var evL2 = function(){};
+
+				var count = 0;
+
+				ee.on('newListener', function(evName, evListener) {
+					evListener === evL1 && ++count;
+					evListener === evL2 && ++count;
+				});
+
+				ee.on(eventName, evL1);
+				ee.on(eventName, evL2);
+				ee.on(eventName, evL1);
+				ee.on(eventName, evL1);
+				ee.on(eventName, evL2);
+				ee.on(eventName, evL2);
+
+				count.should.equal(2);
+			});
 		});
 
-		it('same type of listeners - fired once', function() {
-			var ee = new EE;
-			var eventName = 'eventName';
-			var evL1 = function(){};
-			var evL2 = function(){};
+		describe('removeListener', function() {
+			it('check arguments', function(done) {
+				var ee = new EE;
 
-			var count = 0;
+				var eventName = 'eventName';
+				var eventListener = function(){};
 
-			ee.on('newListener', function(evName, evListener) {
-				evListener === evL1 && ++count;
-				evListener === evL2 && ++count;
+				ee.on('removeListener', function(evName, evListener) {
+					evListener.should.equal(eventListener);
+					evName.should.equal(eventName);
+					done();
+				});
+
+				ee.on(eventName, eventListener);
+				ee.removeListener(eventName, eventListener);
 			});
 
-			ee.on(eventName, evL1);
-			ee.on(eventName, evL2);
-			ee.on(eventName, evL1);
-			ee.on(eventName, evL1);
-			ee.on(eventName, evL2);
-			ee.on(eventName, evL2);
+			it('fired if no more such listeners', function() {
+				var ee = new EE;
 
-			count.should.equal(2);
-		});
-	});
+				var eventName = 'eventName';
+				var eventListener = function() {};
 
-	describe('events - removeListener', function() {
-		it('(event, listener)', function() {
+				var count = 0;
+
+				ee.on('removeListener', function(evName, evListener) {
+					++count;
+				});
+
+				ee.on(eventName, eventListener);
+				ee.on(eventName, eventListener);
+
+				ee.removeListener(eventName, eventListener);
+				count.should.equal(0);
+
+				ee.removeListener(eventName, eventListener);
+				count.should.equal(1);
+			});
 		});
-	});
+	}); 
 });
