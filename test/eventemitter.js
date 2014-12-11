acefb61b7a892286c0bdf3fdbd9267d1f14528bf
petea <smart__petea@mail.ru>
2014-12-11 07:46:01 +0200

    close #36

--- test/eventemitter.js
+++ test/eventemitter.js
@@ -3,6 +3,113 @@ var should = require('should');
 
 describe('EventEmitter', function() {
 	describe('methods', function() {
+		describe('#once', function() {
+			it('(evName, lsn)', function() {
+				var ee = new EE;
+				var eventName = 'eventName';
+				var eventListener = function(){};
+				
+				ee.listeners(eventName).should.be.length(0);
+				ee.once(eventName, eventListener);
+				ee.listeners(eventName).should.be.length(1);
+			});
+
+			it('(evName, [lsn1, lsn2])', function() {
+				var ee = new EE;
+				var eventName = 'eventName';
+				var evLsn1 = function(){};
+				var evLsn2 = function(){};
+
+				ee.listeners(eventName).should.be.length(0);
+				ee.once(eventName, [evLsn1, evLsn2]);
+				ee.listeners(eventName).should.be.length(2);
+			});
+
+			it('({evName1: lsn11, evName2: [lsn21, lsn22, lsn23]})', function() {
+				var ee = new EE;
+				var evName1 = 'evName1';
+				var evName2 = 'evName2';
+				var lsn11 = function(){};
+				var lsn21 = function(){};
+				var lsn22 = function(){};
+				var lsn23 = function(){};
+
+				var params = {};
+
+				params[evName1] = lsn11;
+				params[evName2] = [lsn21, lsn22, lsn23];
+
+				ee.once(params);
+
+				var listeners1 = ee.listeners(evName1);
+				listeners1[0].should.be.equal(lsn11);
+
+				var listeners2 = ee.listeners(evName2);
+				listeners2[0].should.be.equal(lsn21);
+				listeners2[1].should.be.equal(lsn22);
+				listeners2[2].should.be.equal(lsn23);
+			});
+
+			it('[lsn1, lsn2] + lsn3 = [lsn1, lsn2, lsn3] - order preservation', function() {
+				var ee = new EE;
+				var eventName = 'eventName';
+				var evLsn1 = function(){};
+				var evLsn2 = function(){};
+				var evLsn3 = function(){};
+
+				ee.once(eventName, [evLsn1, evLsn2]);
+				ee.once(eventName, evLsn3);
+
+				var listeners = ee.listeners(eventName);
+				listeners[0].should.equal(evLsn1);
+				listeners[1].should.equal(evLsn2);
+				listeners[2].should.equal(evLsn3);
+
+			});
+
+			it('() - throw error', function() {
+				try {
+					(new EE).once();
+				} catch (e) {
+					e.message.should.be.equal("event name is not string");
+				}
+			});
+
+			it('(evName1) - throw error', function() {
+				try {
+					(new EE).once('ok');
+				} catch (e) {
+					e.message.should.be.equal("listeners are not either array or function");
+				}
+			});
+
+			it('fire `removeListener` after call emit', function(done) {
+				var lsn = function(){};
+				var evName = 'ok';
+
+				(new EE)
+					.on('removeListener', function(ev, listener){ 
+						ev.should.equal(evName);
+						listener.should.equal(lsn);
+						done();
+					})
+					.once(evName, lsn)
+					.emit(evName);
+			});
+
+			it('remove listener after call emit', function(done) {
+				var ee = new EE;
+				var evName = 'ok';
+
+				ee
+					.on('removeListener', function() {
+						ee.listeners(evName).should.length(0);
+						done();
+					})
+					.once(evName, function(){})
+					.emit(evName);
+			});
+		});
 		describe('#on', function() {
 			it('(evName, lsn)', function() {
 				var ee = new EE;
